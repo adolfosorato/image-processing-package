1f700cce39f8fee80ce511b64c530ad9fe6e84f1
adolfosorato <adolfosorato@gmail.com>
2024-10-24 00:07:22 -0300

    instalando dist

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="projeto_adolfo",
     version="0.0.1",
     author="adolfosorato",
     author_email="adolfosorato@outlook.com",
